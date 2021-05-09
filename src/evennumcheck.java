724e938a580983637b23ee2330e854f5f9c7cb88
Aditya <adityakumar.sahu1995@gmail.com>
2021-05-09 04:06:12 +0000

    java code

---
+++ src/evennumcheck.java
@@ -0,0 +1,17 @@
+package test;
+import java.util.scanner;
+
+public static void main(string[] args)
+{
+Scanner sc=new Scanner(sysytem.in);
+System.out.println("enter the number");
+int number=sc.nextInt();
+if(number%2)==0
+{
+system.out.println("number is even");
+}
+else
+{
+system.out.println("number is odd");
+}
+}
